f80b92fb3e2446cf2c1b4a03a5140f60d5c154d0
Sasha Chepurniy <sloth04ua@gmail.com>
2021-11-18 06:49:30 +0300

    update after comments v2: added method which creates all variations using only requirement Power plant

--- main.py
+++ main.py
@@ -6,46 +6,40 @@ import xlsxwriter
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
-owners = {'ПрАТ «Укргідроенерго» 62X5590123794668': ['DNIP1HPP аРВЧ_з', 'DNIP1HPP аРВЧ_с',
-                                                     'SEREDHPP аРВЧ_з', 'SEREDHPP аРВЧ_с',
-                                                     'KANIVHPP аРВЧ_з', 'KANIVHPP аРВЧ_с',
-                                                     'KAKHHPP аРВЧ_з',  'KAKHHPP аРВЧ_с',
-                                                     'DNIP2HPP аРВЧ_з', 'DNIP2HPP аРВЧ_с',
-                                                     'KREMHPP аРВЧ_з',  'KREMHPP аРВЧ_с',
-                                                     'KYIVHPP аРВЧ_з',  'KYIVHPP аРВЧ_с',
-                                                     'DNISTHPP рРВЧ_з'],
-          'ПрАТ «Харківська ТЕЦ-5» 56XQ00005D36E00G': ['KHAR5CHPP аРВЧ_р', 'KHAR5CHPP аРВЧ_с',
-                                                       'KHAR5CHPP РПЧ_с','KHAR5CHPP рРВЧ_з',
-                                                       'KHAR5CHPP рРВЧ_р'],
-          'ТОВ «ДТЕК СХІДЕНЕРГО» 56X000000001590J': ['KURTPP аРВЧ_з', 'KURTPP аРВЧ_c',
-                                                     'KURTPP аРВЧ_p', 'KURTPP РПЧ_с',
-                                                     'LUHTPP РПЧ_с'],
-          'АТ «ДТЕК ДНІПРОЕНЕРГО» 21X000000001335A': ['ZAPTPP РПЧ_с', 'ZAPTPP аРВЧ_р',
-                                                      'ZAPTPP аРВЧ_с', 'ZAPTPP аРВЧ_з',
-                                                      'PRYDNTPP рРВЧ_з', 'KRYVTPP рРВЧ_з'],
-          'АТ «ДТЕК ЗАХІДЕНЕРГО» 23X-UKR-ZAKHID-4': ['BURSHTPP-BEI аРВЧ_з', 'BURSHTPP-BEI аРВЧ_с',
-                                                     'BURSHTPP-BEI аРВЧ_р', 'BURSHTPP-BEI РПЧ_с',
-                                                     'LADTPP рРВЧ_з', 'DOBTPP рРВЧ_з']}
+
+owners = {'ПрАТ «Укргідроенерго» 62X5590123794668': ['DNIP1HPP', 'SEREDHPP', 'KANIVHPP', 'KAKHHPP',
+                                                         'DNIP2HPP', 'KREMHPP', 'KYIVHPP', 'DNISTHPP'],
+              'ПрАТ «Харківська ТЕЦ-5» 56XQ00005D36E00G': ['KHAR5CHPP'],
+              'ТОВ «ДТЕК СХІДЕНЕРГО» 56X000000001590J': ['KURTPP', 'LUHTPP'],
+              'АТ «ДТЕК ДНІПРОЕНЕРГО» 21X000000001335A': ['ZAPTPP', 'PRYDNTPP', 'KRYVTPP'],
+              'АТ «ДТЕК ЗАХІДЕНЕРГО» 23X-UKR-ZAKHID-4': ['BURSHTPP-BEI', 'LADTPP', 'DOBTPP']}
 
 
-def mod(key, df):
+def mod(key, df, dct_mask):
     writer = pd.ExcelWriter(f'./output/{key}.xlsx', engine='xlsxwriter')
-    for item in owners[key]:
+    for item in dct_mask[key]:
         split_list = item.split()
         df_temp = df.loc[(df['Power plant'] == split_list[0]) & (df['Product type'] == split_list[1])]
         df_temp.loc[df_temp.index.min(), 'Total'] = df_temp['Deniushka'].sum()
         df_temp.to_excel(writer, sheet_name=item, index=False)
     writer.save()
-    # print(dict_df)
 
 
 def main():
+    dict_mask = {}
+    list_query = []
     cwd = os.path.dirname(os.path.abspath(__file__))
     target = os.path.join(cwd, "input", '*.xlsx')
     df = pd.read_excel(glob(target)[0], 0)
     df.loc[~df['Monitoring result'], 'Deniushka'] = 0
     for owner in owners.keys():
-        mod(owner, df)
+        list_name = owners.get(owner)
+        for item in range(len(list_name)):
+            query = ([f'{owners[owner][item]} {x}' for x in set(df[df['Power plant'] == owners[owner][item]]['Product type'])])
+            list_query += query
+        dict_mask[owner] = list_query
+        list_query = []
+        mod(owner, df, dict_mask)
 
 
 if __name__ == '__main__':
